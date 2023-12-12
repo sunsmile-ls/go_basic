f1e2fed21654823885ec800d8be5bc6ca47f7ad1
lis100 <lis100@sany.com.cn>
2023-12-12 18:00:21 +0800

    fix: add sunsmile

---
+++ zap_demo/main_demo.go
@@ -0,0 +1,34 @@
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"go.uber.org/zap"
+// )
+
+// var logger *zap.Logger
+
+// func InitLogger() {
+// 	logger, _ = zap.NewProduction()
+// }
+// func main() {
+// 	InitLogger()
+// 	defer logger.Sync()
+// 	// simpleHttpGet("www.google.com")
+// 	simpleHttpGet("http://www.baidu.com")
+// }
+
+// func simpleHttpGet(url string) {
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		logger.Error("Error fetching url ...",
+// 			zap.String("url", url),
+// 			zap.Error(err))
+// 	} else {
+// 		logger.Info("Success...",
+// 			zap.String("statusCode", resp.Status),
+// 			zap.String("url", url),
+// 		)
+// 	}
+// 	resp.Body.Close()
+// }
